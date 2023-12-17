7d05fdb84e5452c462bdf14c1fdcfe3143ed0070
Lily Yu <py561@nyu.edu>
2023-12-17 10:29:57 -0500

    Added a timer for controling the audio and scaling the clock

---
+++ winterShow/src/ofApp.h
@@ -0,0 +1,90 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGui.h"
+#include "ofxKinect.h"
+#include "ofxOpenCv.h"
+#include "ofxCv.h"
+
+class ofApp : public ofBaseApp{
+    
+public:
+    void setup();
+    void update();
+    void draw();
+    void exit();
+    void keyPressed(int key);
+    void drawProjector(ofEventArgs& args);
+    
+    //change the projector res here
+    #define PROJECTOR_RESOLUTION_X 1920
+    #define PROJECTOR_RESOLUTION_Y 1080
+    
+    
+    /* ----- clock ----- */
+    int currHr;
+    int currMin;
+    float currSec;
+    int oriSec;
+    
+    float hrHandRad = 0.0;
+    float minHandRad = 0.0;
+    float secHandRad = 0.0;
+    float oriSecHandRad = 0.0;
+    
+    void drawClockHand(string hand, float rotation);
+    void drawShadowOnClock(string hand, float rotation, ofFbo fbo, float scale);
+    
+    float clockRad;
+    float hrShadowScale = 1.0;
+    float minShadowScale = 1.0;
+    float secShadowScale = 1.0;
+    
+    
+    /* ----- shadow ----- */
+    ofxPanel gui;
+    
+    ofxKinect kinects[2];
+    int a, b;
+    
+    // kinect 1
+    ofxKinect kinect1;
+    int angle_kinect1;
+    
+    ofParameter<float> nearThreshold_shadow;
+    ofParameter<float> farThreshold_shadow;
+    
+//    ofxCvContourFinder contourFinder_shadow;
+    ofxCv::ContourFinder contourFinder_shadow;
+    ofxCvGrayscaleImage thresholdImg_shadow;
+    
+    ofFbo shadowFbo;
+    ofPixels ShadowPixels;
+    ofTexture ShadowTexture;
+    
+    // kinect 2
+    ofxKinect kinect2;
+    int angle_kinect2;
+    
+    ofParameter<float> nearThreshold_ceiling;
+    ofParameter<float> farThreshold_ceiling;
+    
+    ofxCvColorImage colorImg_ceiling;
+    
+    ofxCvContourFinder contourFinder_ceiling;
+    ofxCvGrayscaleImage thresholdImg_ceiling;
+    
+    float lerpSpeed = 0.1;
+    float lerpPosX = 0;
+    float lerpPosY = 0;
+    
+    ofSoundPlayer clockSound;
+    bool playSound = false;
+    int timer;
+    int currElapsedTime;
+    bool isExecuted = false;
+    
+    ofFbo projectorFbo;
+    
+    bool test;
+};
